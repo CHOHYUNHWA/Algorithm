3b9dd7ceb5535c5b1669f886d8f77944d7c4e0e1
HyunHwa Cho <gusghk115@gmail.com>
2023-04-15 16:23:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 200 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+        System.out.println(solution(a,b));
+}
+        public static String solution(int a, int b){
+            String answer = "";
+            if (a<b){
+                answer = "<";
+            } else if(a>b){
+                answer = ">";
+            } else {
+                answer = "==";
+            }
+            return answer;
+        }
+
+}
