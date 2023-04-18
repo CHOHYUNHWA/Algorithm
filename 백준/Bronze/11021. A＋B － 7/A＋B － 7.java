6045ea3974bcd197f58a45e61882530e403b6296
HyunHwa Cho <gusghk115@gmail.com>
2023-04-18 17:47:49 +0900

    [Bronze V] Title: A+B - 7, Time: 276 ms, Memory: 18688 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = a+b;
+            System.out.println("Case #"+i+": "+c);
+        }
+    }
+}
