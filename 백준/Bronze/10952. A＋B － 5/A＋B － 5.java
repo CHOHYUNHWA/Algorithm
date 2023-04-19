ee4e6681cb8b693c78e5bd4afbc560a0413ccf47
HyunHwa Cho <gusghk115@gmail.com>
2023-04-19 17:05:41 +0900

    [Bronze V] Title: A+B - 5, Time: 220 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -4,17 +4,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer();
         while(true){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            sb.append(a+b).append("\n");
-            if(a == 0 && b == 0) {
+            if(a == 0 || b == 0){
                 break;
             }
+            System.out.println(a+b);
         }
-        sb.deleteCharAt(sb.length()-1);
-        sb.deleteCharAt(sb.length()-1);
-        System.out.println(sb);
     }
 }
