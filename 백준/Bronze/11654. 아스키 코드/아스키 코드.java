6e5dcb7884dca369e142e65b1ee0db54de8b373a
HyunHwa Cho <gusghk115@gmail.com>
2023-04-25 15:33:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println((int)c);
+    }
+}
