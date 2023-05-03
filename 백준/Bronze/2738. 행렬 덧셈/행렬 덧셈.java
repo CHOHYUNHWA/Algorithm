48eb29f593ff1497e8fe22f708baa93c0a5db476
HyunHwa Cho <gusghk115@gmail.com>
2023-05-03 15:32:23 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 180 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -4,18 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[][] arr = new int[2*N][M];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
         for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j< M; j++){
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         for(int i = 0; i< N; i++){
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j< M; j++){
-                arr[i][j] += sc.nextInt();
+                arr[i][j] += Integer.parseInt(st.nextToken());
                 sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
