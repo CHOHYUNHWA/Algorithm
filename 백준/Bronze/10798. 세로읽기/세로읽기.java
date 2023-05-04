8ee5e2c82d6439c3fb9dd790d2609d6d09d30a5c
HyunHwa Cho <gusghk115@gmail.com>
2023-05-04 14:38:55 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -4,23 +4,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
-        Scanner sc = new Scanner(System.in);
-        String[] sArr = new String[5];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[][] cArr = new char[5][15];
-        for(int i = 0; i < sArr.length; i++){
-            sArr[i] = sc.nextLine();
-        }
-        for(int i = 0; i<cArr.length; i++){
-            Arrays.fill(cArr[i], '\0');
-        }
-        for(int i = 0; i< sArr.length; i++){
-            for(int j = 0; j < sArr[i].length(); j++){
-                cArr[i][j] = sArr[i].charAt(j);
+        for(int i = 0; i< cArr.length; i++){
+                char[] tmp = br.readLine().toCharArray();
+                for(int j = 0; j< tmp.length; j++){
+                    cArr[i][j] = tmp[j];
+                }
             }
-        }
-        for(int i = 0; i<15; i++){
+        for(int i = 0 ; i< 15; i++){
             for(int j = 0; j<5; j++){
-                if(cArr[j][i] !='\0'){
+                if(cArr[j][i] != '\0'){
                     sb.append(cArr[j][i]);
                 }
             }
