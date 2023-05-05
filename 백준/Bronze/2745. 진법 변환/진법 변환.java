b59512a327b6ec5a4ff83ece81f143a599484323
HyunHwa Cho <gusghk115@gmail.com>
2023-05-05 16:34:24 +0900

    [Bronze II] Title: 진법 변환, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int a = sc.nextInt();
+        int answer = Integer.parseInt(s,a);
+        System.out.println(answer);
+    }
+}
