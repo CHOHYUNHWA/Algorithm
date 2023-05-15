207a0eb2b71694a0f650019c68f87a28985521fe
HyunHwa Cho <gusghk115@gmail.com>
2023-05-16 00:04:45 +0900

    [Bronze I] Title: 소인수분해, Time: 160 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for(int i = 2; i <= N; i++){
+            if(N%i == 0){
+                N /= i;
+                System.out.println(i);
+                i = i-1;
+            }
+        }
+    }
+}
