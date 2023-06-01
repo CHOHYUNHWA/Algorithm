c0ffff864b8520c137835e0bc893e127ba3d7975
HyunHwa Cho <gusghk115@gmail.com>
2023-06-01 14:32:51 +0900

    [Silver V] Title: 나이순 정렬, Time: 688 ms, Memory: 47796 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -12,16 +12,7 @@ public class Main {
             arr[i][0] = st.nextToken();
             arr[i][1] = st.nextToken();
         }
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if(Integer.parseInt(o1[0]) == Integer.parseInt(o2[0])){
-                return 0;
-                } else {
-                    return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
-                }
-            }
-        });
+        Arrays.sort(arr,(o1,o2) -> Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]));
         for(int i = 0; i< N; i++){
             sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
         }
