f179340346edf4b85ddfb78463ce86124c1543ec
HyunHwa Cho <gusghk115@gmail.com>
2023-06-13 14:13:30 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 312 ms, Memory: 16628 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        boolean[] prime = new boolean[1000001];
+        isPrime(prime);
+        prime[0] = prime[1] = true;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int t = Integer.parseInt(br.readLine());
+            int count = 0;
+            for(int j = 2; j< t; j++){
+                int tmp = t - j;
+                if(tmp == j && !prime[tmp]){
+                    count += 2;
+                } else if(!prime[tmp] && !prime[j]){
+                    count++;
+                }
+            }
+            sb.append(count/2).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static void isPrime(boolean[] prime) {
+        for (int i = 2; i < Math.sqrt(prime.length); i++) {
+            if (prime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
