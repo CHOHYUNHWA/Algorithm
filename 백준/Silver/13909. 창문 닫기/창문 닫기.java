d86504e70fc538ddc9402f776304544dca1fd8f5
HyunHwa Cho <gusghk115@gmail.com>
2023-06-14 11:17:50 +0900

    [Silver V] Title: 창문 닫기, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -5,18 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        long N = Integer.parseInt(br.readLine());
-        int minCount = 1;
-        int maxCount = 3;
-        int tmp = 1;
-        while(true){
-             if(N >= (long) minCount *tmp && N <= (long) maxCount *tmp){
-                 System.out.println(tmp);
-                 break;
-             }
-             minCount++;
-             maxCount++;
-             tmp++;
-        }
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int)Math.sqrt(N));
     }
 }
