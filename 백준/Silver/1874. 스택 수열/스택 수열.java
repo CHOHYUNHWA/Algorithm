0b014e815a7c68f43ff45bd4eb190b44e7c7112d
HyunHwa Cho <gusghk115@gmail.com>
2023-06-23 13:40:20 +0900

    [Silver II] Title: 스택 수열, Time: 400 ms, Memory: 29064 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -19,6 +19,8 @@ public class Main {
                 } else if(stack.empty() || stack.peek() <= element) {
                         stack.push(count++);
                         sb.append("+").append('\n');
+                    } else {
+                        break;
                     }
             }
         }
