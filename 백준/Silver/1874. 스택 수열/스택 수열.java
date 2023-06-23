94f8d302632152f87d490d464597927b94ecb3c8
HyunHwa Cho <gusghk115@gmail.com>
2023-06-23 18:13:57 +0900

    [Silver II] Title: 스택 수열, Time: 392 ms, Memory: 28056 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,24 +7,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
         int count = 1;
         for(int i = 0; i< N; i++) {
             int element = Integer.parseInt(br.readLine());
             for (int j = 0; j <= element; j++) {
-                    if (!stack.empty() && element == stack.peek()) {
-                    stack.pop();
+                    if (list.size() !=0 && element == list.get(list.size()-1)) {
+                    list.remove(list.size()-1);
                     sb.append("-").append('\n');
                     break;
-                } else if(stack.empty() || stack.peek() <= element) {
-                        stack.push(count++);
+                } else if(list.size() == 0 || list.get(list.size()-1) <= element) {
+                        list.add(count++);
                         sb.append("+").append('\n');
                     } else {
                         break;
                     }
             }
         }
-        if(!stack.isEmpty()){
+        if(list.size() != 0){
             System.out.println("NO");
         } else {
             System.out.println(sb);
