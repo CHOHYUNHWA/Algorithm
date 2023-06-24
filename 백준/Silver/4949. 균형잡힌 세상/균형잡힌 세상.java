0d7160bd1d859ad271c678718a1941ad933d0201
HyunHwa Cho <gusghk115@gmail.com>
2023-06-24 17:11:48 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 260 ms, Memory: 19156 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            String str = br.readLine();
+            if(str.equals(".")){
+                break;
+            }
+            if(match(str)){
+                sb.append("yes").append('\n');
+            } else{
+                sb.append("no").append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+    static boolean match(String str){
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0 ; i < str.length() ; i++){
+            if(!str.contains("(") && !str.contains("[") && !str.contains("]") && !str.contains(")")){
+                return true;
+            }
+            if(str.charAt(i) == '[' || str.charAt(i) == '('){
+                stack.push(str.charAt(i));
+            } else if (str.charAt(i) == ']'){
+                if(stack.empty()){
+                    return false;
+                }
+                if(stack.peek() != '['){
+                    return false;
+                } else if(stack.peek() == '['){
+                    stack.pop();
+                }
+            }
+
+            else if (str.charAt(i) == ')'){
+                if(stack.empty()){
+                    return false;
+                }
+                if(stack.peek() != '('){
+                    return false;
+                } else if(stack.peek() == '('){
+                    if(stack.empty()){
+                        return false;
+                    }
+                    stack.pop();
+                }
+            }
+        }
+        if(!stack.empty()){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
