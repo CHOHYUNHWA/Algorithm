277158fffd32e521f2833ebd0e6d75dbd314ef3b
HyunHwa Cho <gusghk115@gmail.com>
2023-07-01 20:37:39 +0900

    [Gold V] Title: AC, Time: 768 ms, Memory: 88004 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -3,71 +3,74 @@ import java.io.*;
 
 
 public class Main {
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            boolean flag = true;
-            boolean errorFlag = true;
-            Deque<Integer> list = new LinkedList();
             String p = br.readLine();
             int n = Integer.parseInt(br.readLine());
             String arr = br.readLine();
-
             String[] strArr = arr.substring(1,arr.length()-1).split(",");
 
-            if(!strArr[0].equals("")){
-                for(int i = 0; i < strArr.length; i++){
-                    list.add(Integer.parseInt(strArr[i]));
-                }
-            }
+            AC(p,strArr);
 
-                for(int i = 0; i < p.length(); i++) {
-                    if (p.charAt(i) == 'R') {
-                        if (flag) {
-                            flag = false;
-                        } else {
-                            flag = true;
-                        }
-                    }
-                    if (p.charAt(i) == 'D') {
-                        if(list.size() == 0){
-                            sb.append("error").append('\n');
-                            errorFlag = false;
-                            break;
-                        }
-                        else if (flag) {
-                            list.removeFirst();
-                        } else {
-                            list.removeLast();
-                        }
-                    }
-                }
-            if(!errorFlag){
-                errorFlag = true;
-                continue;
-            }
-                sb.append("[");
-
-                if(list.size() != 0) {
-                    int size = list.size();
-                    if(flag){
-                        for(int i = 0; i < size-1; i++){
-                            sb.append(list.getFirst()).append(",");
-                            list.removeFirst();
-                        }
-                        sb.append(list.getLast());
-                    } else {
-                        for(int i = size-1; i > 0; i--){
-                            sb.append(list.getLast()).append(",");
-                            list.removeLast();
-                        }
-                        sb.append(list.getFirst());
-                    }
-                }
-                sb.append("]").append('\n');
         }
         System.out.println(sb);
+    }
+
+
+    static void AC(String p, String[] strArr) {
+
+        boolean reverse = false;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        if(!strArr[0].equals("")){
+            for(int i = 0; i < strArr.length; i++){
+                deque.add(Integer.parseInt(strArr[i]));
+            }
+        }
+        for(int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == 'R') {
+                if (!reverse) {
+                    reverse = true;
+                } else {
+                    reverse = false;
+                }
+            }
+            if (p.charAt(i) == 'D') {
+                if(deque.size() == 0){
+                    sb.append("error").append('\n');
+                    return;
+                }
+                else if (!reverse) {
+                    deque.removeFirst();
+                } else {
+                    deque.removeLast();
+                }
+            }
+        }
+        
+        printAnswer(deque,reverse);
+        
+    }
+
+    static void printAnswer(ArrayDeque deque, boolean reverse){
+        sb.append("[");
+        if(deque.size() != 0) {
+            int size = deque.size();
+            if(!reverse){
+                for(int i = 0; i < size-1; i++){
+                    sb.append(deque.pollFirst()).append(",");
+                }
+                sb.append(deque.pollFirst());
+            } else {
+                for(int i = size-1; i > 0; i--){
+                    sb.append(deque.pollLast()).append(",");
+                }
+                sb.append(deque.pollLast());
+            }
+        }
+        sb.append("]").append('\n');
     }
 }
