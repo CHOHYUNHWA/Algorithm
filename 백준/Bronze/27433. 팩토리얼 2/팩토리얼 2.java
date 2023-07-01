c26fb71264a90fb4afc4c9fc9582fb115b7440e2
HyunHwa Cho <gusghk115@gmail.com>
2023-07-02 02:06:41 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -9,10 +9,13 @@ public class Main {
         System.out.println(factorial(N));
     }
 
-    private static long factorial(int n) {
-        if(n == 0){
-            return 1;
+
+    private static long factorial(int n){
+        long result = 1;
+        for(int i = 2; i <= n; i++){
+            result *= i;
         }
-        return n * factorial(n - 1);
+        return result;
     }
 }
+    
