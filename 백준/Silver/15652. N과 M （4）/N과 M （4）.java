b125d3d28b72922b318259ee7f6ce62e0a267d00
HyunHwa Cho <gusghk115@gmail.com>
2023-07-08 12:06:45 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int[] arr;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(1,0);
+        System.out.println(sb);
+
+
+    }
+
+    private static void dfs(int start, int depth) {
+        if(depth == M){
+            for(int var : arr){
+                sb.append(var).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i = start; i <= N; i++){
+            arr[depth] = i;
+            dfs(i,depth +1);
+        }
+    }
+}
