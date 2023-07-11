63b5982d4711b11aee98c23114ef5e9c36b8dbf6
HyunHwa Cho <gusghk115@gmail.com>
2023-07-11 14:35:11 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int sum = 0;
+        int answer = 0;
+        for (int a : arr){
+            sum += a;
+            answer += sum;
+        }
+        System.out.println(answer);
+    }
+}
