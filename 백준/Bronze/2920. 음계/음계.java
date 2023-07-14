804ba9d866bcb0c2321bac73646ab712938208fe
HyunHwa Cho <gusghk115@gmail.com>
2023-07-15 02:09:05 +0900

    [Bronze II] Title: 음계, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[8];
+        for(int i = 0; i < 8; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        String answer = "";
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i+1] == arr[i]+1) {
+                answer = "ascending";
+            }else if(arr[i]-1 == arr[i+1]){
+                answer = "descending";
+            } else {
+                answer = "mixed";
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
