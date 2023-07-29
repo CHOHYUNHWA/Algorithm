97b9b341ff5e9495ce96d9b0b8bd6647e4742890
HyunHwa Cho <gusghk115@gmail.com>
2023-07-29 19:13:55 +0900

    [Silver III] Title: 파도반 수열, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -2,26 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static Long[] DP = new Long[101];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        while(T-- > 0){
+        while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
-            long[] DP = new long[N+1];
-            if(N <= 3){
-                sb.append(1).append('\n');
-                continue;
-            }
-            DP[0] = 0;
+
+            DP[0] = 0L;
             DP[1] = 1L;
             DP[2] = 1L;
 
-            for(int i = 3; i <= N; i++){
-                DP[i] = DP[i-2] + DP[i-3];
-            }
+            DP[N] = padovan(N);
             sb.append(DP[N]).append('\n');
         }
         System.out.println(sb);
+    }
+
+    static long padovan(int N) {
+        if (DP[N] == null) {
+            DP[N] = padovan(N - 3) + padovan(N - 2);
+        }
+        return DP[N];
     }
 }
