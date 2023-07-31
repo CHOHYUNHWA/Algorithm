29ae425e5c81cb353dae0c80cd472cd8ff6c0eac
HyunHwa Cho <gusghk115@gmail.com>
2023-07-31 15:42:27 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18380 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[1] = 0;
+        if(n>= 2) {
+            dp[2] = 1;
+            if(n>=3) {
+                dp[3] = 1;
+            }
+        }
+        for(int i = 4; i<= n; i++){
+            if(i%6 ==0){
+                dp[i] = Math.min(dp[i/3], Math.min(dp[i/2], dp[i-1]))+1;
+            } else if(i%3 ==0){
+                dp[i] = Math.min(dp[i/3], dp[i-1])+1;
+            } else if(i%2 ==0){
+                dp[i] = Math.min(dp[i/2], dp[i-1])+1;
+            } else {
+                dp[i] = dp[i-1] + 1;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
