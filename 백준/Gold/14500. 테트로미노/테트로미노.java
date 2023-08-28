5f49c4ec91c3824b349a17e5edadb394ec995b78
HyunHwa Cho <gusghk115@gmail.com>
2023-08-28 14:59:45 +0900

    [Gold IV] Title: 테트로미노, Time: 756 ms, Memory: 35148 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+    static int N;
+    static int M;
+    static int max = Integer.MIN_VALUE;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for(int i = 0 ; i < N; i ++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                visited[i][j] = true;
+                dfs(i,j,map[i][j],1);
+                visited[i][j] = false;
+            }
+        }
+        System.out.println(max);
+    }
+
+    private static void dfs(int x, int y,int sum, int count) {
+        if(count == 4){
+            max = Math.max(max,sum);
+            return;
+        }
+
+        for(int i = 0 ;  i < 4; i++){
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if(cx < 0 || cy < 0 || cx >= N || cy >= M){
+                continue;
+            }
+
+            if(!visited[cx][cy]){
+
+                if(count == 2){
+                    visited[cx][cy] = true;
+                    dfs(x,y, sum+map[cx][cy], count+1);
+                    visited[cx][cy] = false;
+                }
+
+                visited[cx][cy] = true;
+                dfs(cx,cy,sum + map[cx][cy], count+1);
+                visited[cx][cy] = false;
+            }
+        }
+    }
+}
