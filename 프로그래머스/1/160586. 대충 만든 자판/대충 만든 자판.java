4d58dda22ddc31230e6b5dce9fdaebd4b8937b8c
HyunHwa Cho <gusghk115@gmail.com>
2023-10-24 17:09:44 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.13 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+class Solution{
+    public int[] solution(String[] keymap, String[] targets){
+        int[] answer = new int[targets.length];
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < keymap.length; i++){
+            for(int j = 0; j < keymap[i].length(); j++) {
+                if (map.get(keymap[i].charAt(j)) == null || map.get(keymap[i].charAt(j)) > j +1) {
+                    map.put(keymap[i].charAt(j), j + 1);
+                }
+            }
+        }
+        for(int i = 0; i < targets.length; i++){
+            int count = 0;
+            for(int j = 0; j < targets[i].length(); j++){
+                if (map.get(targets[i].charAt(j))!=null) {
+                    count += map.get(targets[i].charAt(j));
+                }
+                if(map.get(targets[i].charAt(j)) == null){
+                    count = 0;
+                    break;
+                }
+            }
+            if(count == 0){
+                answer[i] = -1;
+            } else {
+                answer[i] = count;
+            }
+        }
+        return answer;
+    }
+}
