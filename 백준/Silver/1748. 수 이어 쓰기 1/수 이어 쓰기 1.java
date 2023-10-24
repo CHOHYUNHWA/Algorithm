30503a6663173761aeac56a9004f8f18b1751188
HyunHwa Cho <gusghk115@gmail.com>
2023-10-24 15:31:11 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 208 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 
@@ -7,29 +8,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int result = 0;
-        int tmp = 1;
-
+        int count = 1;
+        int length = 10;
+        
         for(int i = 1; i <= N; i++){
-            if(i > 9 && i < 100){
-                tmp = 2;
-            } else if(i > 99 && i < 1_000){
-                tmp = 3;
-            } else if(i > 999 && i < 10_000){
-                tmp = 4;
-            } else if(i > 9_999 && i < 100_000){
-                tmp = 5;
-            } else if(i > 99_999 && i < 1_000_000){
-                tmp = 6;
-            } else if(i > 999_999 && i < 10_000_000){
-                tmp = 7;
-            } else if(i > 9_999_999 && i < 100_000_000){
-                tmp = 8;
-            } else if(i > 99_999_999){
-                tmp = 9;
+            if(i == length){
+                count++;
+                length *= 10;
             }
-
-            result += tmp;
+            result += count;
         }
+        
         System.out.println(result);
     }
 }
