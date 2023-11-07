313d57909eea43e333c12531cdcadae08ca6755a
HyunHwa Cho <gusghk115@gmail.com>
2023-11-07 11:17:09 +0900

    [Silver I] Title: 언더프라임, Time: 3156 ms, Memory: 16100 KB -BaekjoonHub

---
+++ 백준/Silver/1124. 언더프라임/언더프라임.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int max = 100_000;
+    static boolean[] isPrime = new boolean[max+1];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        findPrime();
+        int result = 0;
+        for(int i = A; i <= B; i++){
+            int tmp = 2;
+            int count = 0;
+            int element = i;
+            while(element != 1){
+                if(element % tmp == 0){
+                    element = element/tmp;
+                    count++;
+                    continue;
+                }
+                tmp++;
+            }
+            if(!isPrime[count]){
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+    static void findPrime(){
+        isPrime[0] = isPrime[1] = true;
+        for(int i = 2; i <= Math.sqrt(isPrime.length); i++){
+            if(isPrime[i]){
+                continue;
+            }
+            for(int j = i*i; j < isPrime.length; j= j+i){
+                isPrime[j]= true;
+            }
+        }
+    }
+}
