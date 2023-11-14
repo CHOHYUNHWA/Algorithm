3ca407e73b930054cdbb6795d0b2dc99e600e0ac
HyunHwa Cho <gusghk115@gmail.com>
2023-11-14 12:05:33 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 256 ms, Memory: 22544 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        boolean flag = false;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < str.length(); i++){
+            if(str.charAt(i) == '<'){
+                while(!stack.isEmpty()){
+                    sb.append(stack.pop());
+                }
+                flag = true;
+            } else if(str.charAt(i) == '>'){
+                flag = false;
+                sb.append(str.charAt(i));
+                continue;
+            }
+            if(flag){
+                sb.append(str.charAt(i));
+            } else if(!flag){
+                if(str.charAt(i) == ' '){
+                    while(!stack.isEmpty()){
+                        sb.append(stack.pop());
+                    }
+                    sb.append(' ');
+                    continue;
+                } else {
+                    stack.push(str.charAt(i));
+                }
+            }
+            if (i == str.length() - 1) {
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
